e48e82aa7a9c1f7996437b359360d3d0b252b8c9
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2025-01-01 00:36:14 +0800

    Add content states

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 function Home() {
+    const [content, setContent] = useState("");
+    const [display, setDisplay] = useState("");
+
+    const handleClick = () => {
+      // Set display to current content
+      setDisplay(content);
+    };
+
     return (
       // No need for full-page wrapper - that's in App.tsx
       <div className="space-y-6 w-[90%] md:w-[70%] lg:w-[50%] mx-auto">
@@ -12,10 +22,32 @@ function Home() {
         <textarea
         className="w-full h-64 px-4 py-2 rounded-md text-white focus:ring-0 border-none"
         placeholder="Enter your email content here"
+        // e is an event object
+        onChange={(e) => setContent(e.target.value)}
+        // We need the below because onChange handler causes a re-render
+        // Meaning to say that React will not know of the true value of content
+        // The textbox will still show the content but that is managed by the DOM
+        value={content}
         />
-        <button className="text-left">
-          Click here
+        {/* ${} is an embedded expression. Anything inside is treated as a javascript expression.
+        When you use embedded expression, use `` */}
+        <button className={`text-left ${content ? 'bg-black-500 hover:bg-black-700' : 'bg-gray-400 cursor-not-allowed'}`} onClick={handleClick}>
+          Convert email
         </button>
+
+        {display && (
+        // When you return an element, there should only be one container
+        // So when you have 2 dives, you have to wrap it in a fragment
+        <>
+        <div className="w-full">
+          <p className="text-left text-gray-600">This is the formalised email:</p>
+        </div>
+        <div className="w-full bg-white p-4 rounded-md shadow-md">
+          {/* whitespace pre wrap maintains the formatting of the content */}
+          <p className="text-left text-gray-600 whitespace-pre-wrap">{display}</p>
+        </div>
+        </>
+        )}
       </div>
     )
   }
