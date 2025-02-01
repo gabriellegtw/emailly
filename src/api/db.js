bed7300409a9e9ce2320baba6935496e0a0e8e0b
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2025-02-01 18:08:38 +0800

    Update database config

--- src/api/db.js
+++ src/api/db.js
@@ -9,11 +9,16 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: join(__dirname, '../../.env') });
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
+  connectionString: process.env.DATABASE_URL, // Use connection string
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false, // Required for Render
 });
+
+// const pool = new Pool({
+//   user: process.env.DATABASE_USER,
+//   host: process.env.DATABASE_HOST,
+//   database: process.env.DATABASE_NAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   port: process.env.DATABASE_PORT,
+// });
 
 export default pool;
